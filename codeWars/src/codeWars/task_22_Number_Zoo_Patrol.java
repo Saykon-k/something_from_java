847962d47542e5db4b9f09d61f7ba0eeb2dd05fc
Konstantin <saykovkon@gmail.com>
2020-04-17 19:50:44 +0300

    ds

---
+++ codeWars/src/codeWars/task_22_Number_Zoo_Patrol.java
@@ -0,0 +1,30 @@
+package codeWars;
+import java.math.*;
+import java.util.Arrays;
+public class task_22_Number_Zoo_Patrol {
+
+	public static void main(String[] args) {
+		int u[] = {2, 3, 4};
+		System.out.println(findMissingNumber(u));
+
+	}
+	public static int findMissingNumber_secut(int[] numbers) {
+		BigInteger sum_dig = BigInteger.valueOf(0);
+        BigInteger a ;
+        BigInteger b ;
+        for(int u = 0; u < numbers.length;u++) sum_dig = sum_dig.add(BigInteger.valueOf(numbers[u]));
+        a = BigInteger.valueOf(numbers.length+1);
+        b = BigInteger.valueOf(numbers.length+2);
+        a = a.multiply(b);
+        a = a.divide(BigInteger.valueOf(2));
+        a = a.subtract(sum_dig);
+        return a.intValue(); 
+    }
+	  public static int findMissingNumber_memory_out(int[] numbers) {
+	        return  (int)((numbers.length+1)*(numbers.length+2) / 2 - Arrays.stream(numbers).sum());
+	    }
+	    public static int findMissingNumber(int[] numbers) {
+	        long sum = numbers.length + 1;
+	        return (int)(sum * (sum+1) / 2 - Arrays.stream(numbers).sum());
+	    }
+}
